5583077569d490bb9731c5aed3dd0cdeaeb44480
Doha MAGHNI <doha.maghni99@gmail.com>
2023-11-22 11:50:36 +0100

    first commit

---
+++ src/main/java/com/example/MD_STORE/MdStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.MD_STORE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MD_STORE/MdStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MD_STORE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
